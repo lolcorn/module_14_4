2bf5849d9f78a7e82a237e6a714b940216c5cb6f
Sergey Vovk <lolcorn-19@yandex.ru>
2025-01-05 20:01:46 +0700

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+def initiate_db():
+    connection = sqlite3.connect('DB_Product_module_14_4.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+    );
+    ''')
+
+    connection.commit()
+    connection.close()
+
+def check_db(id, title, description, price):
+    connection = sqlite3.connect('DB_Product_module_14_4.db')
+    cursor = connection.cursor()
+
+    check_db = cursor.execute('SELECT * FROM Products WHERE title=?', (title,))
+
+    if check_db.fetchone() is None:
+        cursor.execute(f'''
+    INSERT INTO Products (id, title, description, price) VALUES('{id}', '{title}', '{description}', '{price}')
+''')
+    connection.commit()
+    connection.close()
+
+def get_all_products():
+    connection = sqlite3.connect('DB_Product_module_14_4.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Products WHERE id > ?', (0,))
+    data_ = cursor.fetchall()
+    connection.commit()
+
+    return data_
+
+
+if __name__ == '__main__':
+    initiate_db()
+    check_db(1,'Продукт 1', 'описание 1', 100)
+    check_db(2,'Продукт 2', 'описание 2', 200)
+    check_db(3,'Продукт 3', 'описание 3', 300)
+    check_db(4, 'Продукт 4', 'описание 4', 400)
+
+
